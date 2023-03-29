76b651c3566a5759cadedd19f041e9f22d9c87a4
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-03-29 11:59:11 -0700

    ./0-strcat

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
-#include “main.h”
+#include "main.h"
 /**
-*  _strncpy – Copies at most an inputted number
+* _strncpy - Copies at most an inputted number
 * of bytes from string src into dest.
 * @dest: The buffer storing the string copy.
 * @src: The source string.
