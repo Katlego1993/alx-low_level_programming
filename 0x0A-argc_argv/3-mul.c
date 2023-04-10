6d54d66a01a2c18bc3d5164a5685da1bab1d4e5a
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-04-10 12:56:49 -0700

    ./mul 2 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+ 
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
@@ -25,14 +26,9 @@ while (i < len && f == 0)
 {
 if (s[i] == '-')
 ++d;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
-digit = s[i] - '0';
-if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
 digit = s[i] - '0';
 if (d % 2)
 digit = -digit;
@@ -66,9 +62,12 @@ if (argc < 3 || argc > 3)
 printf("Error\n");
 return (1);
 }
+
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
-result = num1 *num2;
+result = num1 * num2;
+
 printf("%d\n", result);
+
 return (0);
 }
