aaafb375f593fb581eab89735c90b3e0a02fefcc
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-04-21 14:42:47 -0700

    3-calc.h

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - prints its own opcodes
  * @argc: number of arguments
@@ -8,31 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-int bytes, i;
-char *arr;
+int bytes, index;
+int (*address)(int, char **) = main;
+unsigned char opcode;
 
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-bytes = atoi(argv[1]);
+bytes - atoi(argv[1]);
 
 if (bytes < 0)
 {
 printf("Error\n");
 exit(2);
 }
-arr = (char *)main;
+for (index = 0; index < bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
 
-for (i = 0, i < bytes; i++)
-{
-if (i == bytes - 1)
-{
-printf("%02hhx\n", arr[i]);
-break;
+if (index == bytes - 1)
+continue;
+printf(" ");
+address++;
 }
-printf("02hhx", arr[i]);
-}
+printf("\n");
 return (0);
 }
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,7 +4,7 @@
  * get_op_func - selects the correct function to perform
  * the operation asked by the user
  * @s: The operator passed as argument
- *
+ * @strcmp: value
  * Return: A pointer to the functon correspnding
  * to the operator given as a parameter
  */
@@ -18,3 +18,13 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL},
 };
+int i = 0;
+
+while (i < 5)
+{
+if (strcmp(s, ops[i].op) == 0)
+return (ops[i].f);
+i++;
+}
+return (0);
+}
