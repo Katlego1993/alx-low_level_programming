115e9043f81c0ecf20f4e47cdf8c504359b23562
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-04-21 15:09:27 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,11 +47,6 @@ return (a * b);
  */
 int op_div(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
 return (a / b);
 }
 /**
@@ -63,10 +58,5 @@ return (a / b);
  */
 int op_mod(int a, int b)
 {
-if (b == 0)
-{
-printf("Erro\n");
-exit(100)
-}
 return (a % b);
 }
