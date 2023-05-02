5bca7352db6dfc021113821bb1270448a74ce905
katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-05-02 10:44:54 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,19 +3,19 @@
 /**
  * listint_len - Print the number of linked list elements
  *
- * @h: head print
+ * @h: head of listint_t
  *
  * Return: The number of elements
  *
  */
 size_t listint_len(const listint_t *h)
 {
-size_t count = 0;
+size_t i = 0;
 
 while (h != NULL)
 {
 h = h->next;
-count++;
+i++;
 }
-return (count);
+return (i);
 }
