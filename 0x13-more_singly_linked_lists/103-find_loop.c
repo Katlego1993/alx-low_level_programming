3567e4f4da272bd96d5b51803846e5c09856a490
katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-05-04 00:25:27 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -7,24 +7,24 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *tortoise, *hare;
-tortoise = hare = head;
-while (tortoise && hare && hare->next)
+listint_t *tortoise_t, *hare_h;
+tortoise_t = hare_h = head;
+while (tortoise_t && hare_h && hare_h->next)
 {
-tortoise = tortoise->next;
-hare = hare->next->next;
-if (tortoise == hare)
+tortoise_t = tortoise_t->next;
+hare_h = hare_h->next->next;
+if (tortoise_t == hare_h)
 {
-tortoise = head;
+tortoise_t = head;
 break;
 }
 }
-if[!tortoise || !hare || !hare->next]
+if (!tortoise_t || !hare_h || !hare_h->next)
 return (NULL);
-while (tortoise != hare)
+while (tortoise_t != hare_h)
 {
-tortoise = tortoise->next;
-hare = hare->next;
+tortoise_t = tortoise_t->next;
+hare_h = hare_h->next;
 }
-return (hare);
+return (hare_h);
 }
