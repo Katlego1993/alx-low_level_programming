3a7414bcabdbf5cfc76236f44bfc5b97ead63b69
katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-05-04 01:02:31 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
-* print_listint - prints all list of elements
-*
-* @h :Head of link list node
-*
-* Return: node number
+ * print_listint - pritnts all list of elements
+ *
+ * @h :Head of link list node
+ *
+ * Return: node number
 */
 size_t print_listint(const listint_t *h)
 {
-size_t c = 0;
+size_t count = 0;
 
 while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
-c++;
+count++;
 }
-return (c);
+return (count);
 }
