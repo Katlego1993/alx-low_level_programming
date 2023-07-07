539e1a21ae2195170f7d0312a65977172a26f9c8
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-07-07 06:32:09 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,14 +3,16 @@
 /**
  * set_bit - set bit of index 1
  * @n: number of change pointer
- * @index: index of bit 1
+ * @index: index of bit
  *
  * Return: 1 if success, -1 if fail
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+unsigned int i;
 if (index > 63)
 return (-1);
-*n = ((1UL << index) | *n);
+i = 1 << index;
+*n = (*n | i);
 return (1);
 }
