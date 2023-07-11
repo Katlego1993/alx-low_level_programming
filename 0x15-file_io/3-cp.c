5e35a2a18f65d9eedec2ef567511cab529674e88
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-07-11 03:22:25 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *c_buffer(char *f);
-void close_f(int f);
+void close_f(int fd);
 
 /**
  * c_buffer - 1024 bytes of buffer
@@ -12,28 +12,28 @@ void close_f(int f);
  */
 char *c_buffer(char *f)
 {
-char *buffer;
-buffer = malloc(sizeof(char) * 1024);
-if (buffer == NULL)
+char *buf;
+buf = malloc(sizeof(char) * 1024);
+if (buf == NULL)
 {
 dprintf(STDERR_FILENO,
 "Error: can't write to %s\n", f);
 exit(99);
 }
-return (buffer);
+return (buf);
 }
 
 /**
  * close_f - close the file description
- * @f: file of decsription to be closed
+ * @fd: file of decsription to be closed
  */
-void close_f(int f)
+void close_f(int fd)
 {
 int a;
-a = close(f);
+a = close(fd);
 if (a == -1)
 {
-dprintf(STDERR_FILENO, "Error: can't close f %d\n", f);
+dprintf(STDERR_FILENO, "Error: can't close f %d\n", fd);
 exit(100);
 }
 }
@@ -52,37 +52,37 @@ exit(100);
 int main(int argc, char *argv[])
 {
 int frm, to, a, b;
-char *buffer;
+char *buf;
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
-buffer = c_buffer(argv[2]);
+buf = c_buffer(argv[2]);
 frm = open(argv[1], O_RDONLY);
-a = read(frm, buffer, 1024);
+a = read(frm, buf, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
 if (frm == -1 || a == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: can't read from file %s\n", argv[1]);
-free(buffer);
+free(buf);
 exit(98);
 }
-b = write(to, buffer, a);
+b = write(to, buf, a);
 if (to == -1 || b == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: can't write to %s\n", argv[2]);
-free(buffer);
+free(buf);
 exit(99);
 }
-a = read(frm, buffer, 1024);
+a = read(frm, buf, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
 } while (a > 0);
-free(buffer);
+free(buf);
 close_f(frm);
 close_f(to);
 return (0);
