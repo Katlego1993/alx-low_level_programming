1b5fba13a830b528b322790a7b849e2ba291ee6d
simonmuzariri <simonsammuzariri@gmail.com>
2023-07-14 14:51:58 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,40 +2,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints
- * it to POSIX stdout
- * @filename: a pointer to the name of the file
- * @letters: the number of letters the function
- * should read and print
- * Return: the actual number of bytes the function
- * can read and print
+ * read_textfile - text file that print STDOUT
+ * @filename: Name of a text file
+ * @letters: number of letters
+ * Return: Number of bytes 0 when function fails or filename is NULL
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+ssize_t a, b, c;
+char *buffer;
 
-	if (filename == NULL)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
+if (filename == NULL)
+return (0);
+buffer = malloc(sizeof(char) * letters);
 
-	if (buffer == NULL)
-		return (0);
+if (buffer == NULL)
+return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write
-		(STDOUT_FILENO, buffer, r);
+a = open(filename, O_RDONLY);
+b = read(a, buffer, letters);
+c = write(STDOUT_FILENO, buffer, b);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
-	free(buffer);
-	close(o);
+if (a == -1 || b == -1 || c == -1 || c != b)
+{
+free(buffer);
+return (0);
+}
+free(buffer);
+close(a);
 
-	return (w);
+return (c);
 }
