be6e7a0d98d7140e98e15c887a2c6b40c9a72f8a
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-08-17 23:15:15 -0700

    all

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns node of linked list
+ * @head: head of pointer
+ * @index: node search for index
+ * Return: node or null
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int size;
+	dlistint_t *tmp;
+
+	size = 0;
+	if (head == NULL)
+		return (NULL);
+	tmp = head;
+	while (tmp)
+	{
+		if (index == size)
+			return (tmp);
+		size++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
