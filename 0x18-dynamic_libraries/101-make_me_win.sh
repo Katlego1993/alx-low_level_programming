96ac5d691291b2c183b3c1d9770b1de90880fdb5
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-08-23 01:41:02 -0700

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#z!/bin/bash
-wget -P /tmp/ https://raw.github.com/Aysuarex/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+#!/bin/bash
+wget -P /tmp https://github.com/Katlego1993/alx-low_level_programming/rdw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.
