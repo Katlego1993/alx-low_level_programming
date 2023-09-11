c5a27e0892263f4ad7ac9a5507b7c431960f5f1e
Katlego1993 <Ledwaba.katlego.kl@gmail.com>
2023-09-11 02:54:29 -0700

    all

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,24 +1,26 @@
+#include "hash_tables.h"
+
 /**
- * hash_table_create - Creates a hash table
- * @size: array size
+ * hash_table_create - Creates a hash table.
+ * @size: The array size.
  *
- * Return: Null if error occurs otherwise pointer to new hash table
+ * Return: NULL if error otherwise pointer to new hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *htable;
+	hash_table_t *ht;
 	unsigned long int a;
 
-	htable = malloc(sizeof(hash_table_t));
-	if (htable == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	htable->size = size;
-	htable->array = malloc(sizeof(hash__node_t *) * size);
-	if (htable->array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
 	for (a = 0; a < size; a++)
-		htable->array[a] = NULL;
+		ht->array[a] = NULL;
 
-	return (htable);
+	return (ht);
 }
